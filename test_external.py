34e47e9b3075e21ad5c3cdb56597498d460cf08d
amoh-godwin <amohgyebigodwin@gmail.com>
2019-11-14 17:25:29 +0000

    ...

--- test_external.py
+++ test_external.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
-from urllib.parse import quote
+from urllib.parse import quote, quote_plus, unquote_plus
 from headers import Header
 from settings import Sets
 
@@ -23,7 +23,8 @@ def test_php_get_req(req):
     string = 'array(' + str(len(main_splits)) + ')' + ' {\n'
     for requ in main_splits:
         splits = requ.split('=')
-        string += '  ["' + splits[0] + '"]=>\n  string(' + str(len(splits[1])) + ') "' + splits[1] + '"\n'
+        string += '  ["' + splits[0] + '"]=>\n  string(' + \
+        str(len(splits[1])) + ') "' + splits[1] + '"\n'
     string += '}\n'
     ret_value = string
     print('ret: ', ret_value)
@@ -39,3 +40,42 @@ def test_php_get_req(req):
     print('splits: ', str_resp)
     
     assert body_str == ret_value
+
+
+@pytest.mark.parametrize('req', [('ordinary+text'),
+                                 ('4'),
+                                 ('%^&*()_+')])
+def test_php_post_req(req):
+    parsed = quote_plus(req)
+    request = req = 'test_string=' + parsed
+
+    if '&' in req:
+        main_splits = req.split('&')
+    else:
+        main_splits = [req]
+    string = 'array(' + str(len(main_splits)) + ')' + ' {\n'
+    for requ in main_splits:
+        splits = requ.split('=')
+        splits[1] = unquote_plus(splits[1])
+        string += '  ["' + splits[0] + '"]=>\n  string(' + \
+        str(len(splits[1])) + ') "' + splits[1] + '"\n'
+    string += '}\n'
+    ret_value = string+'d'
+    print('ret: ', ret_value)
+
+    req_string = 'POST /_tests/phpPostReq.php HTTP/1.1\r\n\r\n' + request
+
+    head.getRequest(bytes(req_string, 'utf-8'))
+    resp = head.computeResponse()
+
+    str_resp = str(resp, 'UTF-8')
+    splitted = str_resp.split('\r\n\r\n')
+    body_str = splitted[-1]
+    print('splits: ', body_str)
+    
+    assert body_str == ret_value
+
+
+
+
+
